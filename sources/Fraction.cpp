5313b3c0cd2146cea28e7450675e4b7b9475e5cc
aviel <aviel780@gmail.com>
2023-05-18 23:58:11 +0300

    finish final

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -7,12 +7,69 @@
 #include <stdexcept>
 using namespace ariel;
 using namespace std;
-Fraction::Fraction() : numerator(0), denominator(1) {}
+
+void Fraction::reduce() {// take the idea from the internt
+  if (denominator == 0) {
+    cerr << "the demointor canot be 0" << endl;
+    return;
+  }
+
+  if (numerator == 0) {
+    denominator = 1;
+    cerr << "the frucsion value is 0" << endl;
+    return;
+  }
+
+  int pos_neg = 1;
+
+  if (numerator < 0) { 
+    pos_neg *= -1;
+    numerator *= -1;
+  }
+
+  if (denominator < 0) {
+    pos_neg *= -1;
+    denominator *= -1;
+  }
+
+  int div = gcd(numerator, denominator);
+  numerator = pos_neg * numerator / div;
+  denominator = denominator / div;
+}
+
+Fraction Fraction::floatToFraction(float numerat) { // taken from gpt
+  int decimal_places = 0;
+  float temp = numerat;
+
+  // Count the number of decimal places
+  while (temp != floor(temp)) {
+    decimal_places++;
+    temp *= 10;
+  }
+
+  // Multiply numerator and denominator by 10^decimal_places
+  float numeratt = numerat * pow(10, decimal_places);
+  float denominat = pow(10, decimal_places);
+
+  // Simplify the fraction
+  int gcd_ = gcd(numeratt, denominat);
+  numeratt /= gcd_;
+  denominat /= gcd_;
+
+  return Fraction(numeratt,denominat);
+}
 
 Fraction::Fraction(int numerat, int denominat) : numerator(numerat), denominator(denominat) {
   if (denominat == 0) {
     throw invalid_argument("its invalid to creat frucsion with 0 in the denominator");
   }
+  reduce();
+}
+Fraction::Fraction() : numerator(0), denominator(1) {}
+
+Fraction::Fraction(float numerat) {
+  numerator =round(numerat * 1000);
+  denominator = 1000;
   reduce();
 }
 
@@ -25,33 +82,16 @@ Fraction::Fraction(float numerat, float denominat) : numerator(numerat), denomin
   reduce();
 }
 
-Fraction::Fraction(float numerat) {
-  numerator =round(numerat * 1000);
-  denominator = 1000;
-  reduce();
+int Fraction::gcd(int a, int b) { //taken feom gpt
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
 }
 
-Fraction Fraction::operator+(const Fraction &other) {
-  if ((long long)numerator * other.denominator + other.numerator * denominator >numeric_limits<int>::max()) {
-      throw overflow_error("Invalid, its to big");}
-
-  if((long long)numerator * other.denominator + other.numerator * denominator < numeric_limits<int>::min()){
-      throw overflow_error("Invalid, its to small");}
-  
-  if((long long)denominator * other.denominator > numeric_limits<int>::max()){
-    throw overflow_error("Invalid, the denominator to big");
-  }
-
-  if((long long)numerator * other.numerator > numeric_limits<int>::max()){
-    throw overflow_error("Invalid, the numerator to big");
-  }
-  
-  float commen_denominat = lcm(denominator,other.denominator);
-  float numerat = ((float)numerator * ((float)commen_denominat/(float)denominator)) + 
-  ((float)other.numerator * ((float)commen_denominat/(float)other.denominator));
-  reduce();
-  return Fraction(numerat, commen_denominat);
-}
+int Fraction::lcm(int a, int b) {//taken feom gpt
+  return ((a * b) / gcd(a, b));
+};
 
 Fraction Fraction::operator-(const Fraction &other) {
   if ((long long)numerator * other.denominator - other.numerator * denominator > numeric_limits<int>::max() ){
@@ -84,6 +124,30 @@ Fraction Fraction::operator*(const Fraction &other) {
   return Fraction(numerat, denominat);
 }
 
+Fraction Fraction::operator+(const Fraction &other) {
+  if ((long long)numerator * other.denominator + other.numerator * denominator >numeric_limits<int>::max()) {
+      throw overflow_error("Invalid, its to big");}
+
+  if((long long)numerator * other.denominator + other.numerator * denominator < numeric_limits<int>::min()){
+      throw overflow_error("Invalid, its to small");}
+  
+  if((long long)denominator * other.denominator > numeric_limits<int>::max()){
+    throw overflow_error("Invalid, the denominator to big");
+  }
+
+  if((long long)numerator * other.numerator > numeric_limits<int>::max()){
+    throw overflow_error("Invalid, the numerator to big");
+  }
+  
+  float commen_denominat = lcm(denominator,other.denominator);
+  float numerat = ((float)numerator * ((float)commen_denominat/(float)denominator)) + 
+  ((float)other.numerator * ((float)commen_denominat/(float)other.denominator));
+  reduce();
+  return Fraction(numerat, commen_denominat);
+}
+
+
+
 Fraction Fraction::operator/(const Fraction &other) {
   if (other.numerator == 0 ){
     throw runtime_error("error cant be whane sapre between 2 frucsion");
@@ -115,15 +179,30 @@ bool Fraction::operator==(const Fraction &other) const {
   return flag;
 }
 
-bool Fraction::operator!=(const Fraction &other) const {
-  return !(*this == other);
-}
 
 bool Fraction::operator>(const Fraction &other) const {
   bool flag = false;
 
   float commen_denominat = lcm(denominator,other.denominator);
   if ((float)numerator * ((float)commen_denominat/(float)denominator) >
+   other.numerator * ((float)commen_denominat/(float)other.denominator)){
+    flag = true;
+  }
+
+
+  return flag;
+}
+
+bool Fraction::operator!=(const Fraction &other) const {
+  return !(*this == other);
+}
+
+
+bool Fraction::operator>=(const Fraction &other) const {
+  bool flag = false;
+
+  float commen_denominat = lcm(denominator,other.denominator);
+  if ((float)numerator * ((float)commen_denominat/(float)denominator) >=
    other.numerator * ((float)commen_denominat/(float)other.denominator)){
     flag = true;
   }
@@ -145,18 +224,7 @@ bool Fraction::operator<(const Fraction &other) const {
   return flag;
 }
 
-bool Fraction::operator>=(const Fraction &other) const {
-  bool flag = false;
 
-  float commen_denominat = lcm(denominator,other.denominator);
-  if ((float)numerator * ((float)commen_denominat/(float)denominator) >=
-   other.numerator * ((float)commen_denominat/(float)other.denominator)){
-    flag = true;
-  }
-
-
-  return flag;
-}
 
 bool Fraction::operator<=(const Fraction &other) const {
   bool flag = false;
@@ -176,85 +244,29 @@ Fraction &Fraction::operator++() {
   reduce();
   return *this;
 }
+Fraction &Fraction::operator--() {
+  numerator -= denominator;
+  reduce();
+  return *this;
+}
 
 Fraction Fraction::operator++(int) {
   Fraction newfrac(*this);
   ++(*this);
   return newfrac;
 }
-Fraction &Fraction::operator--() {
-  numerator -= denominator;
-  reduce();
-  return *this;
-}
+
 Fraction Fraction::operator--(int) {
   Fraction newfrac(*this);
   --(*this);
   return newfrac;
 }
 
-void Fraction::reduce() {// take the idea from the internt
-  if (denominator == 0) {
-    cerr << "the demointor canot be 0" << endl;
-    return;
-  }
 
-  if (numerator == 0) {
-    denominator = 1;
-    cerr << "the frucsion value is 0" << endl;
-    return;
-  }
-
-  int pos_neg = 1;
-
-  if (numerator < 0) { 
-    pos_neg *= -1;
-    numerator *= -1;
-  }
-
-  if (denominator < 0) {
-    pos_neg *= -1;
-    denominator *= -1;
-  }
-
-  int div = gcd(numerator, denominator);
-  numerator = pos_neg * numerator / div;
-  denominator = denominator / div;
-}
 
 
 int Fraction::getNumerator() { return this->numerator; }
 int Fraction::getDenominator() { return this->denominator;}
 
-int Fraction::gcd(int a, int b) { //taken feom gpt
-    if (b == 0) {
-        return a;
-    }
-    return gcd(b, a % b);
-}
 
-int Fraction::lcm(int a, int b) {//taken feom gpt
-  return ((a * b) / gcd(a, b));
-};
 
-Fraction Fraction::floatToFraction(float numerat) { // taken from gpt
-  int decimal_places = 0;
-  float temp = numerat;
-
-  // Count the number of decimal places
-  while (temp != floor(temp)) {
-    decimal_places++;
-    temp *= 10;
-  }
-
-  // Multiply numerator and denominator by 10^decimal_places
-  float numeratt = numerat * pow(10, decimal_places);
-  float denominat = pow(10, decimal_places);
-
-  // Simplify the fraction
-  int gcd_ = gcd(numeratt, denominat);
-  numeratt /= gcd_;
-  denominat /= gcd_;
-
-  return Fraction(numeratt,denominat);
-}
